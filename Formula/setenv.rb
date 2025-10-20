4cf355d918f4e7452869a1514cec7212a7a6c19b
jz <jonathan@stilyoapps.com>
2025-10-20 12:24:28 +0300

    Add Homebrew formula

--- Formula/setenv.rb
+++ Formula/setenv.rb
@@ -1,7 +1,7 @@
 class Setenv < Formula
   desc "Simple CLI to manage persistent environment variables in terminal sessions"
-  homepage "https://github.com/jzier/homebrew-setenv"
-  url "https://github.com/jzier/homebrew-setenv/archive/refs/tags/v1.0.0.tar.gz"
+  homepage "https://github.com/jzier/setenv"
+  url "https://github.com/jzier/setenv/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
