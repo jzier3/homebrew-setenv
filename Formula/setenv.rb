ece6845e517f1f4ec19077ca60a090a363022378
jz <jonathan@stilyoapps.com>
2025-10-20 13:41:13 +0300

    bump version

--- Formula/setenv.rb
+++ Formula/setenv.rb
@@ -2,7 +2,7 @@ class Setenv < Formula
   desc "CLI tool to manage global environment variables across shell sessions"
   homepage "https://github.com/jzier3/setenv"
   url "https://github.com/jzier3/setenv/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "ebce7cefc9a07f4a326bbf7587cf65027d427b5787707f8abfc014248fc2ba9c"
+  sha256 "41f75c6de31215ce4345b63e4829d8ce9f227fc1f24c4d4c197f1137513d34c8"
   license "MIT"
 
   def install
