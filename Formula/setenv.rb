6360d0b21808bbdf5db09bc0e2ef003ce34fa972
jz <jonathan@stilyoapps.com>
2025-10-20 12:41:36 +0300

    Add setenv CLI v1.0.1

--- Formula/setenv.rb
+++ Formula/setenv.rb
@@ -1,7 +1,7 @@
 class Setenv < Formula
   desc "Simple CLI to manage persistent environment variables in terminal sessions"
   homepage "https://github.com/jzier3/setenv"
-  url "https://github.com/jzier3/setenv/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/jzier3/setenv/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "19e2d5141850e4803f43018e53da655dd23c7fdf1518e6f79d242b08646c7fc6"
   license "MIT"
 
